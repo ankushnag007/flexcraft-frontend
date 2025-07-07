b1acc6bf89e342305433b8f4eac8d0e818bad9f0
Ankushcode007 <ankushnag786@gmail.com>
2025-07-07 22:29:40 +0530

    working on next

--- flexcraft-app/src/app/IntegrationPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const IntegrationPage = () => {
-  return (
-    <div>IntegrationPage</div>
-  )
-}
-
-export default IntegrationPage
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useRef, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { 
