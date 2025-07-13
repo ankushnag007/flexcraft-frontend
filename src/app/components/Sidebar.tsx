6906e23fd89853c034a08ab71f202977f2390efc
Ankushcode007 <ankushnag786@gmail.com>
2025-07-13 20:27:47 +0530

    theme added

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -78,7 +78,7 @@ const Header = () => {
         sliderElement.style.width = `${width}px`;
       } else {
         // First frame - set the transition
-        sliderElement.style.transition = 'all 150ms cubic-bezier(0.4, 0, 0.2, 1)';
+        // sliderElement.style.transition = 'all 150ms cubic-bezier(0.4, 0, 0.2, 1)';
         
         // Next frame - apply the transform
         requestAnimationFrame(() => {
