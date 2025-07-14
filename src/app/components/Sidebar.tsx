16a349a1d5f06769e1225ffbff98a5bf356fe403
Ankushcode007 <ankushnag786@gmail.com>
2025-07-14 09:12:50 +0530

    theme added

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -78,7 +78,6 @@ const Header = () => {
         sliderElement.style.width = `${width}px`;
       } else {
         // First frame - set the transition
-        
         sliderElement.style.transition = 'all 150ms cubic-bezier(0.4, 0, 0.2, 1)';
         
         // Next frame - apply the transform
