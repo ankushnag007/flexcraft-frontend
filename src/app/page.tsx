5e7664f2700f77a70c461a1abd792dbc0168dab1
Ankushcode007 <ankushnag786@gmail.com>
2025-07-23 18:15:54 +0530

    worked on registration step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Homepage from './(public)/home/page'
 import { redirect } from 'next/navigation'
 
 export default function RootPage() {
-  const isAuthenticated = false; // Replace with your actual auth check
+  const isAuthenticated = true; // Replace with your actual auth check
 
   if (isAuthenticated) {
     redirect('/homepage')
